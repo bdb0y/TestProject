823557a1a7b5dab5a7ac399f6b7b236b2c9a54ed
ali <bandar3eda021@gmail.com>
2020-06-11 00:04:41 +0430

    changed yml file

--- app/src/main/java/com/amin/testproject/MainActivity.java
+++ app/src/main/java/com/amin/testproject/MainActivity.java
@@ -1,9 +1,9 @@
 package com.amin.testproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
