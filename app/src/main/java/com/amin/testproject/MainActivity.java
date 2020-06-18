c950987efb9038715590c619c2752113b5e91593
ali <bandar3eda021@gmail.com>
2020-06-18 23:07:01 +0430

    wanna show how failing affects the badge

--- app/src/main/java/com/amin/testproject/MainActivity.java
+++ app/src/main/java/com/amin/testproject/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Toast.makeText(this, "Hello world",Toast.LENGTH_LONG).show();
+        Toast.makeText(, "Hello world",Toast.LENGTH_LONG).show();
     }
 }
